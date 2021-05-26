b5603cdea0e72743ef40ac95fd5c32c4c326a389
Desarollo <Desarollo@Juanchope>
2021-05-25 22:32:36 -0500

    Creacion de interface -Iniciar Sesion

---
+++ ACADEMIC_THINGS_WEB/src/java/com/controlador/Interface_Sesion.java
@@ -0,0 +1,19 @@
+package com.controlador;
+
+import com.modulo.Usuario;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Juanchope
+ * @author Ivan
+ * 
+ */
+public abstract class Interface_Sesion {
+    
+    private  String 
+            nombre_usuario,
+            contrasenna;
+    
+     public abstract void IniciarSesion();
+}
